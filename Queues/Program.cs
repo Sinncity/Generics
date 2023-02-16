b25ef028cdd028571d3cd2e230fca10ee8d84917
Tparker85 <Pawka8855@gmail.com>
2023-02-15 22:21:04 -0700

    learning queues

---
+++ Queues/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Queues
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // FIFO First in First Out
+
+            // DEQueue = (Removing from the head for Queue)
+            //ENQueue = (ADDing to the Tail for Queue) 
+
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Heather");
+            queue.Enqueue("Terrance");
+            queue.Enqueue("Amiyah");
+            queue.Enqueue("Justin");
+
+            //queue.Dequeue();
+            //queue.Dequeue();
+
+            Console.WriteLine(queue);
+            Console.WriteLine(queue.Contains("Justin"));
+            Console.WriteLine(queue.Peek());
+
+
+
+        }
+    }
+}
